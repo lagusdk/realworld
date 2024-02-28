961430e7f8fbe8fdd0c4995fe768115c9aaa7f3b
lagusdk <llahaoooo@gmail.com>
2024-02-28 16:25:39 +0900

    11_240228

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
-import Routes from './routes';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';;
+import Home from './routes/Home';
+import Login from './routes/login';
+import Signup from './routes/signup';
 
 const App: React.FC = () => {
-  return <Routes />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
--- src/routes/signup.tsx
+++ src/routes/signup.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './style.css';
+import 'style.css';
 
 const Signup: React.FC = () => {
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
